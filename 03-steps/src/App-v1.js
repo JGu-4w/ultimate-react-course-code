0663046dcba52993369dd791d6b2df8a149582f6
Joe Gu <joegu.4work@gmail.com>
2024-04-25 02:03:45 +0800

    feat(03-steps): use children prop in button component

---
+++ 03-steps/src/App-v1.js
@@ -0,0 +1,72 @@
+import { useState } from 'react'
+
+const messages = [
+  'Learn React ⚛️',
+  'Apply for jobs 💼',
+  'Invest your new income 🤑',
+]
+
+function App() {
+  return (
+    <div>
+      <Steps />
+      {/* <Steps /> */}
+    </div>
+  )
+}
+
+function Steps() {
+  const [step, setStep] = useState(1)
+  const [isOpen, setIsOpen] = useState(true)
+
+  function handlePrevious() {
+    if (step > 1) setStep(step - 1)
+  }
+
+  function handleNext() {
+    if (step < 3) setStep(step + 1)
+  }
+
+  return (
+    <div>
+      <button className="close" onClick={() => setIsOpen(!isOpen)}>
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? 'active' : ''}>1</div>
+            <div className={step >= 2 ? 'active' : ''}>2</div>
+            <div className={step >= 3 ? 'active' : ''}>3</div>
+          </div>
+
+          <div className="message">
+            Step {step}: {messages[step - 1]}
+          </div>
+
+          <div className="buttons">
+            <Button textColor="#fff" bgColor="#7950f2" onClick={handlePrevious}>
+              <span>👈</span> Previous
+            </Button>
+            <Button textColor="#fff" bgColor="#7950f2" onClick={handleNext}>
+              Next <span>👉</span>
+            </Button>
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
+
+function Button({ textColor, bgColor, onClick, children }) {
+  return (
+    <button
+      style={{ color: textColor, backgroundColor: bgColor }}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  )
+}
+
+export default App
