bd51a40d58ffa04d19d4d03577de357ecda1dade
Joe Gu <joegu.4work@gmail.com>
2024-04-25 11:31:45 +0800

    feat(03-steps): restruct step message into reusable component

--- 03-steps/src/App-v1.js
+++ 03-steps/src/App-v1.js
@@ -40,9 +40,7 @@ function Steps() {
             <div className={step >= 3 ? 'active' : ''}>3</div>
           </div>
 
-          <div className="message">
-            Step {step}: {messages[step - 1]}
-          </div>
+          <StepMessage step={step}>{messages[step - 1]}</StepMessage>
 
           <div className="buttons">
             <Button textColor="#fff" bgColor="#7950f2" onClick={handlePrevious}>
@@ -54,6 +52,15 @@ function Steps() {
           </div>
         </div>
       )}
+    </div>
+  )
+}
+
+function StepMessage({ step, children }) {
+  return (
+    <div className="message">
+      <h3>Step {step}</h3>
+      {children}
     </div>
   )
 }
