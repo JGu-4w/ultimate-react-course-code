b3903e074cd811e30f1c7f75d1354a96221ca658
Joe Gu <joegu.4work@gmail.com>
2024-04-25 14:07:04 +0800

    feat(07-accordion): use children prop to show text

---
+++ 07-accordion/src/App-v1.js
@@ -0,0 +1,62 @@
+import { useState } from 'react'
+import './styles.css'
+
+const faqs = [
+  {
+    title: 'Where are these chairs assembled?',
+    text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus.',
+  },
+  {
+    title: 'How long do I have to return my chair?',
+    text: 'Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus.',
+  },
+  {
+    title: 'Do you ship to countries outside the EU?',
+    text: 'Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!',
+  },
+]
+
+export default function App() {
+  return (
+    <div>
+      <Accordion></Accordion>
+    </div>
+  )
+}
+
+function Accordion() {
+  const [curOpen, setCurOpen] = useState(null)
+
+  return (
+    <div className="accordion">
+      {faqs.map((item, i) => (
+        <AccordionItem
+          key={item.title}
+          num={i}
+          title={item.title}
+          curOpen={curOpen}
+          onOpen={setCurOpen}
+        >
+          <div className="content-box">{item.text}</div>
+        </AccordionItem>
+      ))}
+    </div>
+  )
+}
+
+function AccordionItem({ num, title, curOpen, onOpen, children }) {
+  const isOpen = num === curOpen
+
+  function handleToggle() {
+    onOpen(isOpen ? null : num)
+  }
+
+  return (
+    <div className={`item ${isOpen ? 'open' : ''}`} onClick={handleToggle}>
+      <p className="number">{String(num + 1).padStart(2, '0')}</p>
+      <p className="title">{title}</p>
+      <p className="icon">{isOpen ? '-' : '+'}</p>
+      {isOpen && children}
+    </div>
+  )
+}
