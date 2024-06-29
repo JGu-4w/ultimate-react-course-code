6d340b7568d3d2c23f89ca79110780fa2395b80e
Joe Gu <joegu.4work@gmail.com>
2024-06-29 16:46:06 +0800

    feat: add json-server; add Question and Options component;

---
+++ 14-react-quiz/src/components/App.js
@@ -0,0 +1,67 @@
+import { useEffect, useReducer } from 'react'
+
+import Header from './Header'
+import Main from './Main'
+import Loader from './Loader'
+import Error from './Error'
+import StartScreen from './StartScreen'
+import Question from './Question'
+
+const initialState = {
+  questions: [],
+  // 'loaidng', 'error', 'ready', 'active', 'finished'
+  status: 'loading',
+  index: 0,
+}
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'dataReceived':
+      return {
+        ...state,
+        questions: action.payload,
+        status: 'ready',
+      }
+    case 'dataFailed':
+      return {
+        ...state,
+        status: 'error',
+      }
+    case 'start':
+      return { ...state, status: 'active' }
+
+    default:
+      break
+  }
+}
+
+export default function App() {
+  const [{ questions, status, index }, dispatch] = useReducer(
+    reducer,
+    initialState
+  )
+
+  const questionsCount = questions.length
+
+  useEffect(() => {
+    fetch('http://localhost:8000/questions')
+      .then((res) => res.json())
+      .then((res) => dispatch({ type: 'dataReceived', payload: res }))
+      .catch((error) => dispatch({ type: 'dataFailed' }))
+  }, [])
+
+  return (
+    <div className="app">
+      <Header />
+
+      <Main>
+        {status === 'loading' && <Loader />}
+        {status === 'error' && <Error />}
+        {status === 'ready' && (
+          <StartScreen count={questionsCount} dispatch={dispatch} />
+        )}
+        {status === 'active' && <Question question={questions[index]} />}
+      </Main>
+    </div>
+  )
+}
