81bb14bac79caa62e57210b6721bf6c3a2fe43f1
Joe Gu <joegu.4work@gmail.com>
2024-07-02 00:45:47 +0800

    feat: try css modules to set scoped style

--- 16-worldwise/src/pages/Homepage.jsx
+++ 16-worldwise/src/pages/Homepage.jsx
@@ -1,10 +1,14 @@
+import { Link } from 'react-router-dom'
 import PageNav from '../components/PageNav'
+import AppNav from '../components/AppNav'
 
 export default function Homepage() {
   return (
     <div>
       <PageNav />
-      Homepage
+      <AppNav />
+      <h1>Homepage</h1>
+      <Link to="app">Go to the app</Link>
     </div>
   )
 }
