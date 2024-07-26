a5d4c7a782b114494d62a793bee548a1e224a567
Joe Gu <joegu.4work@gmail.com>
2024-07-26 14:03:06 +0800

    feat(19-reduex-intro):  init project to try redux

---
+++ 19-redux-intro/src/store.js
@@ -0,0 +1,49 @@
+import { createStore } from 'redux'
+
+const initialState = {
+  balance: 0,
+  loan: 0,
+  loanPurpose: '',
+}
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case 'account/deposit':
+      return { ...state, balance: state.balance + action.payload }
+    case 'account/withdraw':
+      if (action.payload > state.balance) return state
+      return { ...state, balance: state.balance - action.payload }
+    case 'account/requestLoan':
+      if (state.loan > 0) return state
+      // LATER
+      return {
+        ...state,
+        loan: action.payload.loan,
+        loanPurpose: action.payload.purpose,
+        balance: state.balance + action.payload.loan,
+      }
+    case 'account/payLoan':
+      return {
+        ...state,
+        loan: 0,
+        loanPurpose: '',
+        balance: state.balance - state.loan,
+      }
+    default:
+      return state
+  }
+}
+
+const store = createStore(reducer)
+
+store.dispatch({ type: 'account/deposit', payload: 500 })
+console.log(store.getState())
+store.dispatch({ type: 'account/withdraw', payload: 200 })
+console.log(store.getState())
+store.dispatch({
+  type: 'account/requestLoan',
+  payload: { loan: 1000, purpose: 'Buy a car.' },
+})
+console.log(store.getState())
+store.dispatch({ type: 'account/payLoan' })
+console.log(store.getState())
