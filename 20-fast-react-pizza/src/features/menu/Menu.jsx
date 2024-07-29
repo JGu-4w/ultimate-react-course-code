7622756da6291435b4429ed132233160a1fa5d08
Joe Gu <joegu.4work@gmail.com>
2024-07-30 01:21:12 +0800

    feat(20-fast-react-pizza): error handling in Form Actions

--- 20-fast-react-pizza/src/features/menu/Menu.jsx
+++ 20-fast-react-pizza/src/features/menu/Menu.jsx
@@ -1,22 +1,21 @@
-import { useLoaderData } from 'react-router-dom'
-import MenuItem from './MenuItem'
-import { getMenu } from '../../services/apiRestaurant'
+import { useLoaderData } from "react-router-dom";
+import MenuItem from "./MenuItem";
+import { getMenu } from "../../services/apiRestaurant";
 
 function Menu() {
-  const menu = useLoaderData()
-  console.log(menu)
+  const menu = useLoaderData();
   return (
     <ul>
       {menu.map((pizza) => (
         <MenuItem key={pizza.id} pizza={pizza} />
       ))}
     </ul>
-  )
+  );
 }
 
 export async function loader() {
-  const menu = await getMenu()
-  return menu
+  const menu = await getMenu();
+  return menu;
 }
 
-export default Menu
+export default Menu;
--- 20-fast-react-pizza/src/features/order/CreateOrder.jsx
+++ 20-fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Form, redirect } from "react-router-dom";
+import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
 
 // https://uibakery.io/regex-library/phone-number
@@ -34,6 +34,12 @@ const fakeCart = [
 
 function CreateOrder() {
   // const [withPriority, setWithPriority] = useState(false);
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === "submitting";
+
+  const formErrors = useActionData();
+  console.log(formErrors);
+
   const cart = fakeCart;
 
   return (
@@ -51,6 +57,7 @@ function CreateOrder() {
           <div>
             <input type="tel" name="phone" required />
           </div>
+          {formErrors?.phone && <p>{formErrors.phone}</p>}
         </div>
 
         <div>
@@ -73,7 +80,9 @@ function CreateOrder() {
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <button>Order now</button>
+          <button disabled={isSubmitting}>
+            {isSubmitting ? "Placing order..." : "Order now"}
+          </button>
         </div>
       </Form>
     </div>
@@ -89,6 +98,13 @@ export async function action({ request }) {
     cart: JSON.parse(data.cart),
     priority: data.priority === "on",
   };
+
+  const errors = {};
+  if (!isValidPhone(order.phone))
+    errors.phone =
+      "Please give us your correct phone number. We might need it to contact you.";
+
+  if (Object.keys(errors).length > 0) return errors;
 
   const newOrder = await createOrder(order);
   return redirect(`/order/${newOrder.id}`);
