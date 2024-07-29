9a9deebb2694edbb97314fcbe07780bca842b395
Joe Gu <joegu.4work@gmail.com>
2024-07-29 23:50:57 +0800

    feat(20-fast-react-pizza): try useNagivation to get nagivation state

--- 20-fast-react-pizza/src/ui/AppLayout.jsx
+++ 20-fast-react-pizza/src/ui/AppLayout.jsx
@@ -1,16 +1,19 @@
 import Header from './Header'
+import Loader from './Loader'
 import CartOverview from '../features/cart/CartOverview'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useNavigation } from 'react-router-dom'
 
 export default function AppLayout() {
+  const navigation = useNavigation()
+  const isLoading = navigation.state === 'loading'
+  console.log(navigation)
   return (
-    <div>
+    <div className="layout">
+      {isLoading && <Loader />}
       <Header />
-
       <main>
         <Outlet />
       </main>
-
       <CartOverview />
     </div>
   )
