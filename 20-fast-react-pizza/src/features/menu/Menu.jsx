f7250d34f0958d55a14bcdab85b74c182120ff89
Joe Gu <joegu.4work@gmail.com>
2024-08-06 09:55:18 +0800

    feat(20-fast-react-pizza): styling the menu

--- 20-fast-react-pizza/src/features/menu/Menu.jsx
+++ 20-fast-react-pizza/src/features/menu/Menu.jsx
@@ -1,11 +1,11 @@
-import { useLoaderData } from "react-router-dom";
-import MenuItem from "./MenuItem";
-import { getMenu } from "../../services/apiRestaurant";
+import { useLoaderData } from 'react-router-dom';
+import MenuItem from './MenuItem';
+import { getMenu } from '../../services/apiRestaurant';
 
 function Menu() {
   const menu = useLoaderData();
   return (
-    <ul>
+    <ul className="divide-y divide-stone-200 px-2">
       {menu.map((pizza) => (
         <MenuItem key={pizza.id} pizza={pizza} />
       ))}
