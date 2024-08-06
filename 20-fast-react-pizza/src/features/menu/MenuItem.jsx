b4641eef47130fdbf615f4172d175022f8794c83
Joe Gu <joegu.4work@gmail.com>
2024-08-06 16:28:20 +0800

    feat(20-fast-react-pizza): add menu item to cart

--- 20-fast-react-pizza/src/features/menu/MenuItem.jsx
+++ 20-fast-react-pizza/src/features/menu/MenuItem.jsx
@@ -1,15 +1,30 @@
+import { useDispatch } from 'react-redux';
 import Button from '../../ui/Button';
 import { formatCurrency } from '../../utils/helpers';
+import { addItem } from '../cart/cartSlice';
 
 function MenuItem({ pizza }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+  const dispatch = useDispatch();
+
+  function handleAddCart() {
+    const newItem = {
+      pizzaId: id,
+      name,
+      quantity: 1,
+      unitPrice,
+      totalPrice: unitPrice * 1,
+    };
+
+    dispatch(addItem(newItem));
+  }
 
   return (
     <li className="flex gap-4 py-2">
       <img
         src={imageUrl}
         alt={name}
-        className={`h-24 ${soldOut ? 'opacity-70' : 'grayscale'}`}
+        className={`h-24 ${!soldOut ? 'opacity-70' : 'grayscale'}`}
       />
       <div className="flex grow flex-col pt-0.5">
         <p className="font-medium">{name}</p>
@@ -25,7 +40,11 @@ function MenuItem({ pizza }) {
             </p>
           )}
 
-          <Button type="small">Add to cart</Button>
+          {!soldOut && (
+            <Button type="small" onClick={handleAddCart}>
+              Add to cart
+            </Button>
+          )}
         </div>
       </div>
     </li>
